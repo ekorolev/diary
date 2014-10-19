4c368d684337fc9227dd15895bc62544a21664a1
ekorolev <ea.korolev@mail.ru>
2014-10-19 10:42:11 +0400

    config

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
 module.exports = {
-	key: "4595935",
-	secret: "TzAV80In7JNE2XmF1t93",
-	redirect: "http://diary.loc/return.html"
+	key: "4595960",
+	secret: "jxDOi3qB3I16tzbhSCJF",
+	redirect: "http://ekor.cloudapp.net/return.html"
 }
+
+// Тестовое приложение: 
+// module.exports = {
+// 	key: "4595935",
+// 	secret: "TzAV80In7JNE2XmF1t93",
+// 	redirect: "http://diary.loc/return.html"
+// }
